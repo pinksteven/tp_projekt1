914d95c6ae2d3c58a73178e5122a83ea917f427f
Steven <st.latuszek@gmail.com>
2025-03-17 16:48:39 +0100

    rebuild the graph data structure

--- main.cpp
+++ main.cpp
@@ -141,134 +141,63 @@ struct Edge {
   // Overloading breaks passing the function and i have no idea how to pass an
   // overloaded one
   static bool cmp(Edge edge1, Edge edge2) {
-    return edge1.weight > edge2.weight;
+    return edge1.weight < edge2.weight;
   };
 };
 
 struct Graph {
-  DynamicList<Edge> edges;
-  DynamicList<unsigned> vertices;
-  DynamicList<DynamicList<bool>> neighbors_matrix;
+  DynamicList<DynamicList<Edge>>
+      body;                // Used for dealing with if it's neighboring
+  DynamicList<Edge> edges; // Holds all the given edges, used for sorting
 
-  void add(unsigned vertex1, unsigned vertex2, unsigned weight) {
-    edges.push({vertex1, vertex2, weight});
+  void add(Edge edge) {
+    edges.push(edge); // For sorting
 
-    bool vertex1_in_list = false;
-    bool vertex2_in_list = false;
-    for (int i = 0; i < vertices.len(); i++) {
-      if (vertices.get(i) == vertex1) {
-        vertex1_in_list = true;
-      }
-      if (vertices.get(i) == vertex2) {
-        vertex2_in_list = true;
-      }
-    }
-    if (!vertex1_in_list) {
-      vertices.push(vertex1);
-    }
-    if (!vertex2_in_list) {
-      vertices.push(vertex2);
-    }
-
-    if (neighbors_matrix.len() <= vertex1) {
-      DynamicList<bool> temp;
-      temp.insert(true, vertex2);
-      neighbors_matrix.insert(temp, vertex1);
+    if (body.len() < edge.vertex1) {
+      DynamicList<Edge> temp;
+      temp.push(edge);
+      body.insert(temp, edge.vertex1);
     } else {
-      neighbors_matrix.get_pointer(vertex1)->data.insert(true, vertex2);
+      body.get(edge.vertex1).push(edge);
     }
-    // It's a rectangle so it should work no matter the vertex order
-    if (neighbors_matrix.len() <= vertex2) {
-      DynamicList<bool> temp;
-      temp.insert(true, vertex1);
-      neighbors_matrix.insert(temp, vertex2);
+
+    if (body.len() < edge.vertex2) {
+      DynamicList<Edge> temp;
+      temp.push(edge);
+      body.insert(temp, edge.vertex2);
     } else {
-      neighbors_matrix.get_pointer(vertex2)->data.insert(true, vertex1);
+      body.get(edge.vertex2).push(edge);
     }
   }
 
   bool are_connected(unsigned vertex1, unsigned vertex2) {
-    return neighbors_matrix.get(vertex1).get(vertex2);
+    DynamicList<Edge> list = body.get(vertex1);
+    for (int i = 0; i < list.len(); i++) {
+      if (list.get(i).vertex2 == vertex2) {
+        return true;
+      }
+    }
+    return false;
   }
 };
 
-Graph rebuild(Graph graph, DynamicList<unsigned> vertices) {
-  Graph new_graph;
-  for (int i = 0; i < graph.edges.len(); i++) {
-    Edge temp = graph.edges.get(i);
-    for (int j = 0; j < vertices.len(); j++) {
-      if (vertices.get(j) == temp.vertex1 || vertices.get(j) == temp.vertex2) {
-        new_graph.add(temp.vertex1, temp.vertex2, temp.weight);
-        break;
-      }
-    }
-  }
-  return new_graph;
-}
-
-// Straight i keep forgetting my logic as i write, subconcious solving idk
-void traverse(Graph graph, unsigned vertex, DynamicList<bool> &visited) {
-  visited.insert(true, vertex);
-  for (int v = 0; v < graph.vertices.len(); v++) {
-    if (v > visited.len()) {
-      visited.insert(false, v);
-    }
-    // Yeah i need the vertex id instead of index so we grab it or something
-    unsigned u = graph.vertices.get(vertex);
-    if (graph.are_connected(u, v) &&
-        (visited.get(v) == false ||
-         visited.get_pointer(v)->initialized == false)) {
-      traverse(graph, v, visited);
-    }
-  }
-}
-
-// I grabbed the algorithm from the internet and modded to fit
-void split_separate(Graph graph, DynamicList<Graph> &push_here) {
-  DynamicList<bool> visited;
-  DynamicList<unsigned> connected;
-  DynamicList<unsigned> leftovers; // imma eat them
-
-  for (int u = 0; u < graph.vertices.len(); u++) {
-    for (int i = 0; i < graph.vertices.len(); i++) {
-      visited.insert(false, i);
-      traverse(graph, u, visited);
-    }
-  }
-  for (int i = 0; i < visited.len(); i++) {
-    if (visited.get(i) == true) {
-      connected.push(graph.vertices.get(i));
-    } else {
-      leftovers.push(graph.vertices.get(i));
-    }
-  }
-  push_here.push(rebuild(graph, connected));
-  if (leftovers.len() != 0) {
-    Graph what_is_this_city = rebuild(graph, leftovers);
-    split_separate(what_is_this_city, push_here);
-  }
-}
-
 int main() {
   int len;
-  Graph list;
-  std::cout << "Array length: ";
+  Graph graph;
+  std::cout << "Amount of edges: ";
   std::cin >> len;
   for (int i = 0; i < len; i++) {
     unsigned vertex1, vertex2, weight;
 
-    std::cout << "Edge in format: vertex1 vertex2 weight" << i + 1 << ": ";
+    std::cout << "Edge " << i + 1 << " in format: vertex1 vertex2 weight:";
     std::cin >> vertex1 >> vertex2 >> weight;
-    list.add(vertex1, vertex2, weight);
+    graph.add({vertex1, vertex2, weight});
   }
 
-  list.edges.sort(&Edge::cmp);
-  len = list.edges.len();
-  std::cout << "Measured length: " << len << std::endl;
-  std::cout << "Sorted array: " << std::endl;
-  for (int i = 0; i < len; i++) {
-    std::cout << list.edges.get(i).vertex1 << " " << list.edges.get(i).vertex2
-              << " " << list.edges.get(i).weight << std::endl;
+  graph.edges.sort(&Edge::cmp);
+  for (int i = 0; i < graph.edges.len(); i++) {
+    std::cout << graph.edges.get(i).vertex1 << " " << graph.edges.get(i).vertex2
+              << " " << graph.edges.get(i).weight << std::endl;
   }
   return 0;
 }
